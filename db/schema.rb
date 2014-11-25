8be03c237eb58cfbcf8223c9bc61f0d679eb10a7
denisesh <denisesh@gmail.com>
2014-11-24 16:14:42 -0800

    organizations too

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124224744) do
+ActiveRecord::Schema.define(version: 20141125001011) do
+
+  create_table "organizations", force: true do |t|
+    t.string   "name"
+    t.integer  "number_of_employees"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", force: true do |t|
     t.string   "first_name"
@@ -19,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141124224744) do
     t.date     "date_of_birth"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "phone"
   end
 
 end
